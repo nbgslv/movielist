0aec7a3991d7c6b52336ed699e63e679b7ed5c10
nbgslv <nbgslv@gmail.com>
2020-04-28 20:55:22 +0300

    added movie presentation with Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Card = () => {
-  return <h2>movie #1</h2>;
+const Card = ({ movie }) => {
+  return (
+    <div>
+      <h2>{`#${movie.ranking} - ${movie.title} (${movie.year})`}</h2>
+      <img src={movie.img.src} alt={movie.img.alt} width="200" />
+      <p>{`Distributor: ${movie.distributor}`}</p>
+      <p>{`Amount: ${movie.amount}`}</p>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  movie: PropTypes.shape({
+    ranking: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    img: {
+      src: PropTypes.string.isRequired,
+      alt: PropTypes.string.isRequired,
+    },
+    distributor: PropTypes.string.isRequired,
+    amount: PropTypes.number.isRequired,
+  }),
+};
+
+Card.defaultProps = {
+  movie: {
+    ranking: 0,
+    title: 'Movie Title',
+    year: 2000,
+  },
 };
 
 export default Card;
